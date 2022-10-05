912ef4b3bb25aa7d8be61cb64adae0961c174577
garliczek <wiktorlasek@onet.pl>
2022-10-05 14:40:50 +0200

    zadania z liczbami

---
+++ numbers.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<?php
+$a=7;
+$b=9;
+$c=3.5;
+$d=4.5;
+$x=5;
+$y=15;
+echo "<Zmienna a ma wartość" .$a. "a funkcja is_int zwraca dla niej wynik";
+var_dump(is_int($a)); echo "<br>";
+echo "Zmienna b ma wartość" .$b. "a funkcja is_int zwraca dla niej wynik";
+var_dump(is_int($b)); echo "<br>";
+echo "Zmienna c ma wartość" .$c. "a funkcja is_int zwraca dla niej wynik";
+var_dump(is_int($c)); echo "<br>";
+echo "Zmienna d ma wartość" .$d. "a funkcja is_int zwraca dla niej wynik";
+var_dump(is_int($d)); echo "<br>";
+echo "<Zmienna a ma wartość" .$a. "a funkcja is_int zwraca dla niej wynik";
+var_dump(is_float($a)); echo "<br>";
+echo "Zmienna b ma wartość" .$b. "a funkcja is_int zwraca dla niej wynik";
+var_dump(is_float($b)); echo "<br>";
+echo "Zmienna c ma wartość" .$c. "a funkcja is_int zwraca dla niej wynik";
+var_dump(is_float($c)); echo "<br>";
+echo "Zmienna d ma wartość" .$d. "a funkcja is_int zwraca dla niej wynik";
+var_dump(is_float($d)); echo "<br>";
+echo "<br> <br> <br>";
+$suma=$x+$y;
+echo "Wynik dodawania $x i $y wynosi $suma". "<br>";
+$suma=$x-$y;
+echo "Wynik odejmowania $x i $y wynosi $suma". "<br>";
+$suma=$x/$y;
+echo "Wynik dzielenia $x i $y wynosi $suma". "<br>";
+$suma=$x%$y;
+echo "Wynik dzielenia z reszta $x i $y wynosi $suma". "<br>";
+echo "<br> <br> <br>";
+$j=4;
+$k=35;
+$suma=$j*$k;
+echo "Pole prostokata o bokach dlugosci $x i $y wynosi $suma j<sup>2". "<br>";
+$suma=$j+$j+$k+$k;
+echo "Obwód porstokąta o bokach $x i $y wynosi $suma". "<br>"
+?>
+</html>
---
+++ php.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<p style="color: aliceblue; background-color: darkblue; font-weight: bold"><?= var_dump(abs(-4.2))?></p>
+<p style="color: aliceblue; background-color: darkblue; font-weight: bold"><?= var_dump(abs(5))?></p>
+<p style="color: aliceblue; background-color: darkblue; font-weight: bold"><?= var_dump(abs(-5))?></p>
+<p style="color: aliceblue; background-color: darkblue; font-weight: bold"><?php echo min(0, 150, 30, 20, -8, -200)?></p>
+<p style="color: aliceblue; background-color: darkblue; font-weight: bold"><?php echo max(0, 150, 30, 20, -8, -200)?></p>
+<p style="color: aliceblue; background-color: darkblue; font-weight: bold"><?php echo pi()?></>
+<p style="color: aliceblue; background-color: darkblue; font-weight: bold"><?= var_dump(round(3.4, 2))?></p>
+<p style="color: aliceblue; background-color: darkblue; font-weight: bold"><?= var_dump(round(3.6, 2))?></p>
+<p style="color: aliceblue; background-color: darkblue; font-weight: bold"><?= var_dump(round(5.045, 2))?></p>
+<p style="color: aliceblue; background-color: darkblue; font-weight: bold"><?= var_dump(round(5.055, 2))?></p>
+<p style="color: aliceblue; background-color: darkblue; font-weight: bold"><?php echo rand(900, 1000)?></p>
+<p style="color: aliceblue; background-color: darkblue; font-weight: bold"><?php echo sqrt(0)?></p>
+<p style="color: aliceblue; background-color: darkblue; font-weight: bold"><?php echo sqrt(64)?></p>
+<p style="color: aliceblue; background-color: darkblue; font-weight: bold"><?php echo sqrt(256)?></p>
+<p style="color: aliceblue; background-color: darkblue; font-weight: bold;"><?= sqrt(65536)?></p>
+</body>
+</html>
